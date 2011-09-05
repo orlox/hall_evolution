24c8fd29df07a26daa2f2888d19a735bc6027d20
Pablo Marchant Campos <pamarca@gmail.com>
2011-09-05 19:59:33 -0400

    Initial Commit.
    Created as_toroidal project to include the evolution of a purely
    toroidal field under the assumption of axial simmetry.

---
+++ as_toroidal/as_toroidal.cpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  as_toroidal.cpp
+ *
+ *    Description:  This code follows the evolution of an axisymmetric purely toroidal
+ *    MF on a NS due to the hall effect.
+ *
+ *        Version:  1.0
+ *        Created:  05/09/11 19:18:47
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Pablo Marchant
+ *        Company:  Pontificia Universidad Católica de Chile
+ *
+ * =====================================================================================
+ */
+
+#include	<stdlib.h>
+#include	<math.h>
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  Sets the initial conditions, runs the simulation loop, and logs
+ *  the results.
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	return EXIT_SUCCESS;
+}				/* ----------  end of function main  ---------- */
