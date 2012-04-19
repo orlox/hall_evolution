eddf89810cec2b42ee3eb7afd1ee1b7a9e5f79dc
Pablo Marchant Campos <pamarca@gmail.com>
2012-04-19 13:46:34 -0300

    Delete purely toroidal code
    -Deleted purely toroidal version, as it is available as a compile option
    for the poloidal+toroidal code.
    -Included calculation of field variation with respect to the equilibrium
    field, not just with respect to the initial configuration.

--- as_full/field_variation.py
+++ as_full/field_variation.py
@@ -15,6 +15,11 @@ kk=7.03266
 def A_Ohm_function(r,th):
     return r*pow(sin(th),2)*(((sin(kk*r)/(kk*r)-cos(kk*r))*A2)/(kk*r)+((-sin(kk*r)-cos(kk*r)/(kk*r))*A1)/(kk*r))
 
+#Hall equilibrium field
+rmin=-0.75
+def A_eq_function(r,th):
+	return 1/2.0*pow(sin(th),2)*((3*pow(rmin,5)-5*pow(rmin,3))/r+5*pow(r,2)-3*pow(r,4))/(2-5*pow(rmin,3)+3*pow(rmin,5));
+
 #Get name of folder with results
 folder=sys.argv[1]
 #add "/" if it is not given in folder name
@@ -53,6 +58,7 @@ dth=math.pi/(thNum-1)
 #Create array to store A and B at each step
 A=zeros((rNum+1,thNum+1));
 A_Ohm=zeros((rNum+1,thNum+1));
+A_eq=zeros((rNum+1,thNum+1));
 B=zeros((rNum+1,thNum+1));
 
 #Fill array with ohm eigenmode for A_Ohm
@@ -61,15 +67,20 @@ for i in range(rNum):
     for j in range(thNum):
         th=j*dth
         A_Ohm[i][j]=A_Ohm_function(r,th)
+        A_eq[i][j]=A_eq_function(r,th)
 
 #Create array to store the vector values of the field at the initial and later times
 #field is solved at midpoints in the grid.
+#B_field_i=initial field
 B_field_i=zeros((rNum,thNum,3));
+#B_field_k=field at each timestep
 B_field_k=zeros((rNum,thNum,3));
+#B_field_Ohm=fundamental Ohm mode, with the same polarity that the equilibrium
 B_field_Ohm=zeros((rNum,thNum,3));
-initial_energy=0;
+#B_field_eq=equilibrium field due to rigid rotation of constant electron density in the shell.
+B_field_eq=zeros((rNum,thNum,3));
 
-#solve vector magnetic field of the fundamental Ohm mode
+#solve vector magnetic field of the fundamental Ohm mode and the equilibrium field
 for i in range(rNum-1):
     r=i*dr+dr/2+rmin
     for j in range(thNum-1):
@@ -77,22 +88,29 @@ for i in range(rNum-1):
         #Solve each component
         #r component
         B_field_Ohm[i][j][0]=1/r/r/sin(th)*(A_Ohm[i][j+1]-A_Ohm[i][j]+A_Ohm[i+1][j+1]-A_Ohm[i+1][j])/2/dth
+        B_field_eq[i][j][0]=1/r/r/sin(th)*(A_eq[i][j+1]-A_eq[i][j]+A_eq[i+1][j+1]-A_eq[i+1][j])/2/dth
         #th component
         B_field_Ohm[i][j][1]=-1/r/sin(th)*(A_Ohm[i+1][j]-A_Ohm[i][j]+A_Ohm[i+1][j+1]-A_Ohm[i][j+1])/2/dr
+        B_field_eq[i][j][1]=-1/r/sin(th)*(A_eq[i+1][j]-A_eq[i][j]+A_eq[i+1][j+1]-A_eq[i][j+1])/2/dr
         #phi component
         B_field_Ohm[i][j][2]=0
+        B_field_eq[i][j][2]=0
 
-#solve energy of Ohm eigenmode
+#solve energy of Ohm eigenmode and equilibrium field
 energy_Ohm=0
+energy_eq=0
 for i in range(rNum-1):
     r=i*dr+dr/2+rmin
     for j in range(thNum-1):
         th=j*dth+dth/2
         energy_Ohm+=(pow(B_field_Ohm[i][j][0],2)+pow(B_field_Ohm[i][j][1],2)+pow(B_field_Ohm[i][j][2],2))*pow(r,2)*sin(th)
+        energy_eq+=(pow(B_field_eq[i][j][0],2)+pow(B_field_eq[i][j][1],2)+pow(B_field_eq[i][j][2],2))*pow(r,2)*sin(th)
 energy_Ohm=energy_Ohm*dr*dth/4
+energy_eq=energy_eq*dr*dth/4
 
 #analyze all data
 k=0
+initial_energy=0;
 while 1:
     #add zeros to the number of the plot, so they are ordered appropately
     num_file=str(k)
@@ -168,30 +186,38 @@ while 1:
                 B_field_i[i][j][1]=B_field_k[i][j][1]
                 B_field_i[i][j][2]=B_field_k[i][j][2]
     
-    #solve integral of deltaB, at each time, field must be corrected to have equal energy than the initial one
+    #solve integrals of the different dB^2, fields must be corrected to have energy equal to 1
     dB_energy=0
-    for i in range(rNum-1):
-        r=i*dr+dr/2+rmin
-        for j in range(thNum-1):
-            th=j*dth+dth/2
-            dB_energy+=(pow(B_field_i[i][j][0]*sqrt(energy_Ohm/initial_energy)-B_field_k[i][j][0]*sqrt(energy_Ohm/energy),2)
-                            +pow(B_field_i[i][j][1]*sqrt(energy_Ohm/initial_energy)-B_field_k[i][j][1]*sqrt(energy_Ohm/energy),2)
-                            +pow(B_field_i[i][j][2]*sqrt(energy_Ohm/initial_energy)-B_field_k[i][j][2]*sqrt(energy_Ohm/energy),2))*pow(r,2)*sin(th)
-    dB_energy=dB_energy*dth*dr/4
-
-    #solve integral of deltaB with respect to Ohm fundamental mode, at each time, field must be corrected to have equal energy than the initial Ohm mode used
     dB_energy_Ohm=0
+    dB_energy_eq=0
     for i in range(rNum-1):
         r=i*dr+dr/2+rmin
         for j in range(thNum-1):
             th=j*dth+dth/2
-            dB_energy_Ohm+=(pow(B_field_Ohm[i][j][0]-B_field_k[i][j][0]*sqrt(energy_Ohm/energy),2)
-                            +pow(B_field_Ohm[i][j][1]-B_field_k[i][j][1]*sqrt(energy_Ohm/energy),2)
-                            +pow(B_field_Ohm[i][j][2]-B_field_k[i][j][2]*sqrt(energy_Ohm/energy),2))*pow(r,2)*sin(th)
+            #solve dB with respect to initial field
+            dB_energy+=     (pow(B_field_i[i][j][0]*sqrt(1/initial_energy)-B_field_k[i][j][0]*sqrt(1/energy),2)
+                            +pow(B_field_i[i][j][1]*sqrt(1/initial_energy)-B_field_k[i][j][1]*sqrt(1/energy),2)
+                            +pow(B_field_i[i][j][2]*sqrt(1/initial_energy)-B_field_k[i][j][2]*sqrt(1/energy),2))*pow(r,2)*sin(th)
+            #solve dB with respect to Ohm fundamental mode
+            dB_energy_Ohm+= (pow(B_field_Ohm[i][j][0]*sqrt(1/energy_Ohm)-B_field_k[i][j][0]*sqrt(1/energy),2)
+                            +pow(B_field_Ohm[i][j][1]*sqrt(1/energy_Ohm)-B_field_k[i][j][1]*sqrt(1/energy),2)
+                            +pow(B_field_Ohm[i][j][2]*sqrt(1/energy_Ohm)-B_field_k[i][j][2]*sqrt(1/energy),2))*pow(r,2)*sin(th)
+            #solve dB with respect to equilibrium field
+            dB_energy_eq+=  (pow(B_field_eq[i][j][0]*sqrt(1/energy_eq)-B_field_k[i][j][0]*sqrt(1/energy),2)
+                            +pow(B_field_eq[i][j][1]*sqrt(1/energy_eq)-B_field_k[i][j][1]*sqrt(1/energy),2)
+                            +pow(B_field_eq[i][j][2]*sqrt(1/energy_eq)-B_field_k[i][j][2]*sqrt(1/energy),2))*pow(r,2)*sin(th)
+    dB_energy=dB_energy*dth*dr/4
     dB_energy_Ohm=dB_energy_Ohm*dth*dr/4
+    dB_energy_eq=dB_energy_eq*dth*dr/4
 
-    f.write(str(k*dt) + " " + str(dB_energy) + " " + str(dB_energy_Ohm)+"\n")
-    print str(num_file)+" "+str(energy)+" "+str(dB_energy)+" "+str(dB_energy_Ohm)
+    #solve integral of deltaB with respect to Ohm fundamental mode, at each time, field must be corrected to have energy equal to 1
+    for i in range(rNum-1):
+        r=i*dr+dr/2+rmin
+        for j in range(thNum-1):
+            th=j*dth+dth/2
+
+    f.write(str(k*dt) + " " + str(dB_energy) + " " + str(dB_energy_Ohm)+ " " + str(dB_energy_eq)+"\n")
+    print str(num_file)+" "+str(energy)+" "+str(dB_energy)+" "+str(dB_energy_Ohm)+" "+str(dB_energy_eq)
     k+=plotSteps
 
 f.close()
